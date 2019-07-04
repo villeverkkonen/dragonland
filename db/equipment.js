ead4eb1b511537bb81551037156dfebd5d9914ac
Ville Verkkonen <ville.verkkonen@solidabis.com>
2019-07-04 15:33:17 +0300

    Changed imageUrl paths

--- db/equipment.js
+++ db/equipment.js
@@ -4,35 +4,35 @@ const equipment = [
     title: "Mighty Sword of Thunder",
     stats: "+2 Attack",
     price: 5,
-    imageUrl: "http://localhost:3001/images/sword.png"
+    imageUrl: "/images/sword.png"
   },
   {
     id: 2,
     title: "Helm of Destruction",
     stats: "+2 Defense",
     price: 5,
-    imageUrl: "http://localhost:3001/images/helmet.png"
+    imageUrl: "/images/helmet.png"
   },
   {
     id: 3,
     title: "Feather Boots of Storm & Wind",
     stats: "+2 Movement",
     price: 5,
-    imageUrl: "http://localhost:3001/images/boots.png"
+    imageUrl: "/images/boots.png"
   },
   {
     id: 4,
     title: "Woodcutting Axe",
     stats: "+1 Attack",
     price: 4,
-    imageUrl: "http://localhost:3001/images/axe.png"
+    imageUrl: "/images/axe.png"
   },
   {
     id: 5,
     title: "Dragon Wing Shield of Protection",
     stats: "+3 Defense",
     price: 6,
-    imageUrl: "http://localhost:3001/images/shield.png"
+    imageUrl: "/images/shield.png"
   }
 ];
 
